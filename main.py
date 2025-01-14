8039c7c32cfa7eb28e65b3bed0cf201dd61ad006
Antonio Rodriguez <antonioinorge@hotmail.com>
2025-01-14 10:35:15 +0100

    create paddle

--- main.py
+++ main.py
@@ -4,21 +4,18 @@ import time
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width = 800, height = 600)
-screen.title("Pong)")
+screen.setup(width = 1300, height = 800)
+screen.title("Breakout)")
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
+paddle = Paddle((0, -320))
 ball = Ball()
 scoreboard = Scoreboard()
 
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkey(paddle.go_right, "Right")
+screen.onkey(paddle.go_left, "Left")
 
 game_on = True
 while game_on:
@@ -29,7 +26,7 @@ while game_on:
     if ball.ycor() > 280 or ball.ycor() < - 280:
         ball.bounce_y()
 
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    if ball.distance(paddle) < 50 and ball.xcor() > 320 or ball.distance(paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
     
     if ball.xcor() > 380:
--- paddle.py
+++ paddle.py
@@ -5,17 +5,17 @@ class Paddle(Turtle):
         super().__init__()
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_wid = 5, stretch_len = 1)
+        self.shapesize(stretch_wid = 1.2, stretch_len =5)
         self.penup()
         self.goto(position)
 
-    def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+    def go_right(self):
+        new_x = self.xcor() + 20
+        self.goto(new_x, self.ycor())
 
-    def go_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+    def go_left(self):
+        new_x = self.xcor() - 20
+        self.goto(new_x, self.ycor())
 
 class Ball(Turtle):
     def __init__(self):
